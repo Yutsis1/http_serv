73ce0855c40ac18bb8ba38d44247d8fc33a7011a
RomanIutsis <yutsis5@gmail.com>
2021-03-09 23:18:00 +0300

    Update echo_server.py. add answer as json body

--- echo_server.py
+++ echo_server.py
@@ -1,17 +1,30 @@
+import json
+import socketserver
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+from typing import Tuple
 
 
 class S(BaseHTTPRequestHandler):
+    _use_text = False
+
     def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        if self._use_text:
+            self.send_header('Content-type', 'text/html')
+        else:
+            self.send_header('Content-type', 'application/json')
         self.end_headers()
 
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        if self._use_text:
+            self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        else:
+            dict_for_answer = {"method": "GET", "content": {"lol": "kek"}}
+            json_str = json.dumps(dict_for_answer)
+            self.wfile.write(json_str.encode(encoding='utf_8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
@@ -20,7 +33,12 @@ class S(BaseHTTPRequestHandler):
                      str(self.path), str(self.headers), post_data.decode('utf-8'))
 
         self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        if self._use_text:
+            self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        else:
+            dict_for_answer = {"method": "POST", "content": {"lol": "kek"}}
+            json_str = json.dumps(dict_for_answer)
+            self.wfile.write(json_str.encode(encoding='utf_8'))
 
 
 def run(server_class=HTTPServer, handler_class=S, port=8081, address='127.0.0.1'):
